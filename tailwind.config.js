6c6be9a92ff5a0b72616e3d308a893fc8702d002
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2024-09-22 10:30:58 +0700

    Page Home

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+      screens: {
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1240px',
+        '2xl': '1240px',
+      },
+  
+      extend: {
+        colors: {
+          primary: '#000000',
+        }
+      },
+  
+      fontFamily: {
+        be: ['Be Vietnam Pro', 'system-ui']
+      }
+    }
+  }
